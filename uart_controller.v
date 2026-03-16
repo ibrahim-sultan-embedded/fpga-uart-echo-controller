930d8f1c75fe66fb6ab98af97796117790f03c66
ibrahim-sultan <itsultan23@gmail.com>
2026-03-17 00:06:13 +0200

    Update uart_controller.v

--- uart_controller.v
+++ uart_controller.v
@@ -1,26 +1,57 @@
-`timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 03/09/2026 03:06:01 PM
-// Design Name: 
-// Module Name: uart_controller
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
-
 module uart_controller(
 
-    );
+    input wire clk,
+    input wire rst,
+
+    input wire rx_done,
+    input wire [7:0] rx_data,
+
+    input wire tx_busy,
+
+    output reg tx_start,
+    output reg [7:0] tx_data
+
+);
+
+localparam IDLE = 2'd0;
+localparam SEND = 2'd1;
+
+reg [1:0] state;
+
+always @(posedge clk or posedge rst) begin
+
+    if(rst) begin
+        state <= IDLE;
+        tx_start <= 0;
+        tx_data <= 0;
+    end
+
+    else begin
+
+        tx_start <= 0;
+
+        case(state)
+
+        IDLE:
+        begin
+            if(rx_done) begin
+                tx_data <= rx_data;
+                state <= SEND;
+            end
+        end
+
+        SEND:
+        begin
+            if(!tx_busy) begin
+                tx_start <= 1;
+                state <= IDLE;
+            end
+        end
+
+        endcase
+
+    end
+
+end
+
 endmodule
