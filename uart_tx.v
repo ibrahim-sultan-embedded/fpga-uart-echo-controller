73fb768b852a2d713d83ee312d1c101be8941e01
ibrahim-sultan <itsultan23@gmail.com>
2026-03-17 00:12:00 +0200

    Update uart_tx.v

--- uart_tx.v
+++ uart_tx.v
@@ -8,35 +8,41 @@ module uart_tx (
     output reg        tx_busy
 );
 
-    reg [3:0] bit_index;
-    reg [9:0] shift_reg;
+reg [3:0] bit_index;
+reg [9:0] shift_reg;
 
-    always @(posedge clk or posedge rst) begin
-        if (rst) begin
-            tx        <= 1'b1;
-            tx_busy   <= 1'b0;
-            bit_index <= 4'd0;
-            shift_reg <= 10'b1111111111;
-        end else begin
-            if (!tx_busy) begin
-                tx <= 1'b1;
-                if (tx_start) begin
-                    shift_reg <= {1'b1, tx_data, 1'b0};
-                    tx_busy   <= 1'b1;
-                    bit_index <= 4'd0;
-                end
-            end else if (baud_tick) begin
-                tx <= shift_reg[0];
-                shift_reg <= {1'b1, shift_reg[9:1]};
+always @(posedge clk or posedge rst) begin
+    if (rst) begin
+        tx        <= 1'b1;
+        tx_busy   <= 1'b0;
+        bit_index <= 4'd0;
+        shift_reg <= 10'b1111111111;
+    end 
+    else begin
 
-                if (bit_index == 4'd9) begin
-                    tx_busy   <= 1'b0;
-                    bit_index <= 4'd0;
-                end else begin
-                    bit_index <= bit_index + 1'b1;
-                end
+        if (!tx_busy) begin
+            tx <= 1'b1;
+
+            if (tx_start) begin
+                shift_reg <= {1'b1, tx_data, 1'b0};
+                tx_busy   <= 1'b1;
+                bit_index <= 0;
+            end
+        end 
+
+        else if (baud_tick) begin
+            tx <= shift_reg[0];
+            shift_reg <= {1'b1, shift_reg[9:1]};
+
+            if (bit_index == 9) begin
+                tx_busy <= 0;
+            end 
+            else begin
+                bit_index <= bit_index + 1;
             end
         end
+
     end
+end
 
 endmodule
