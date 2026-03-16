ae126cdbb15c36767c381ff4ea42b478bfe5a01d
ibrahim-sultan <itsultan23@gmail.com>
2026-03-17 00:14:31 +0200

    Update baud_gen.v

--- baud_gen.v
+++ baud_gen.v
@@ -1,25 +1,25 @@
-module baud_gen
-#(
-    parameter CLK_FREQ = 50000000,
-    parameter BAUD_RATE = 115200
-)
-(
+module baud_gen(
+
     input wire clk,
     input wire rst,
     output reg baud_tick
+
 );
 
-localparam integer BAUD_DIV = CLK_FREQ / BAUD_RATE;
+parameter DIV = 50;
 
-reg [31:0] counter;
+reg [15:0] counter;
 
 always @(posedge clk or posedge rst) begin
-    if (rst) begin
+
+    if(rst) begin
         counter <= 0;
         baud_tick <= 0;
     end
+
     else begin
-        if (counter == BAUD_DIV-1) begin
+
+        if(counter == DIV-1) begin
             counter <= 0;
             baud_tick <= 1;
         end
@@ -27,7 +27,9 @@ always @(posedge clk or posedge rst) begin
             counter <= counter + 1;
             baud_tick <= 0;
         end
+
     end
+
 end
 
 endmodule
